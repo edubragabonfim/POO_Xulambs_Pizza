b4c1788141d48fe4729ad4aec2c4f515ac2bac68
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2025-08-26 19:16:28 -0300

    First Commit

---
+++ POO_N_Xulambs_Pizza/Pizza.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_N_Xulambs_Pizza
+{
+    class Pizza
+    {
+        int _totalIngredientes;
+        int _maximoIngredientes;
+        string _descricao;
+        double _precoBase;
+        double _precoAdicional;
+
+        public Pizza()
+        {
+            _totalIngredientes = 0;
+            _maximoIngredientes = 8;
+            _descricao = "Pizza";
+            _precoBase = 29d;
+            _precoAdicional = 5d;
+        }
+
+        public bool VerificarLimite(int novoTotal)
+        {
+            return novoTotal <= _maximoIngredientes;
+        }
+
+        public int AdicionarIngrediente(int quantidade)
+        {
+            if (!VerificarLimite(_totalIngredientes + quantidade))
+            {
+                _totalIngredientes += quantidade;
+            }
+            return _totalIngredientes;
+        }
+
+    }
+}
---
+++ POO_N_Xulambs_Pizza/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace POO_N_Xulambs_Pizza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
