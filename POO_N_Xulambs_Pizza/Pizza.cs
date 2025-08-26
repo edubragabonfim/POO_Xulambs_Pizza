a2c41f93f6619d3e80116a39663602615e1038b2
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2025-08-26 19:51:00 -0300

    Implementação de alguns métodos + 1 Documentação

--- POO_N_Xulambs_Pizza/Pizza.cs
+++ POO_N_Xulambs_Pizza/Pizza.cs
@@ -23,18 +23,38 @@ namespace POO_N_Xulambs_Pizza
             _precoAdicional = 5d;
         }
 
-        public bool VerificarLimite(int novoTotal)
+        /// <summary>
+        /// Método usado internamento dentro da classe para verificar se a quantidade de ingredientes que serão adicionados é permitido, dados o máximo de ingrdientes possíveiss.
+        /// </summary>
+        /// <param name="quantos">Quantidade de ingredientes a serem adicionados na pizza.</param>
+        /// <returns></returns>
+        private bool VerificarLimite(int quantos)
         {
-            return novoTotal <= _maximoIngredientes;
+            return (quantos > 0 && quantos + _totalIngredientes <= _maximoIngredientes);
         }
 
-        public int AdicionarIngrediente(int quantidade)
+        public int AdicionarIngrediente(int quantos)
         {
-            if (!VerificarLimite(_totalIngredientes + quantidade))
+            if (VerificarLimite(quantos))
             {
-                _totalIngredientes += quantidade;
+                _totalIngredientes += quantos;
             }
             return _totalIngredientes;
+        }
+
+        public string EmitirCupom()
+        {
+            return $"{_descricao} | Valor: {ValorVenda()} | Com {_totalIngredientes} adicionais.";
+        }
+
+        public double ValorAdicional()
+        {
+            return _totalIngredientes * _precoAdicional;
+        }
+
+        public double ValorVenda()
+        {
+            return _precoBase + ValorAdicional();
         }
 
     }
